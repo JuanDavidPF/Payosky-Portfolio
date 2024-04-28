04e0bd1a50e16ed15d44e65e511baf257fb59be3
Juan Payán <juandavidPF@gmail.com>
2024-04-27 23:11:40 -0500

    Update packages and add storybook

---
+++ .storybook/preview.ts
@@ -0,0 +1,16 @@
+import '../app/globals.css'; // replace with the name of your tailwind css file
+
+import type { Preview } from "@storybook/react";
+
+const preview: Preview = {
+  parameters: {
+    controls: {
+      matchers: {
+        color: /(background|color)$/i,
+        date: /Date$/i,
+      },
+    },
+  },
+};
+
+export default preview;
