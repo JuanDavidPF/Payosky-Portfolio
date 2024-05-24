39e9f89aa6c319dc4ffa789a415f847e1be2981a
Juan Payán <juandavidPF@gmail.com>
2024-05-24 14:23:49 -0500

    Run link, update packages and fix tailwind intelisense

--- app/components/atoms/PaysokyLogo/PaysoskyLogo.tsx
+++ app/components/atoms/PaysokyLogo/PaysoskyLogo.tsx
@@ -1,58 +1,60 @@
 import Isotype from "./Isotype";
 import Logotype from "./Logotype";
 
-
-
 export type PayoskyLogoTypeProps = {
-    size: number,
-    strokeColor: string,
-    textColor: string
-}
-
+  size: number;
+  strokeColor: string;
+  textColor: string;
+};
 
 type PayoskyLogoProps = {
-    type?: 'isotype' | 'logotype',
-    theme?: 'light' | 'dark',
-    contrast?: 'sm' | 'md' | 'lg',
-    size?: number,
-}
+  type?: "isotype" | "logotype";
+  theme?: "light" | "dark";
+  contrast?: "sm" | "md" | "lg";
+  size?: number;
+};
 
 const PayoskyLogo: React.FC<PayoskyLogoProps> = ({
-    contrast = 'lg',
-    theme = 'dark',
-    type = "isotype",
-    size = 64
+  contrast = "lg",
+  theme = "dark",
+  type = "isotype",
+  size = 64,
 }) => {
+  const StrokeColor = () => {
+    switch (contrast) {
+      case "sm":
+        return "transparent";
+      case "md":
+        return theme === "dark" ? "#333333" : "#EEEEEE";
+      case "lg":
+        return theme === "dark" ? "#191919" : "#333333";
+    }
+  }; //Closes StrokeColor method
 
-    const StrokeColor = () => {
-        switch (contrast) {
-            case 'sm':
-                return 'transparent';
-            case "md":
-                return theme === 'dark' ? '#333333' : '#EEEEEE';
-            case "lg":
-                return theme === 'dark' ? '#191919' : '#333333';
-        }
-    }//Closes StrokeColor method
+  const TextColor = () => {
+    switch (contrast) {
+      case "sm":
+      case "md":
+        return theme === "dark" ? "#EEEEEE" : "#333333";
+      case "lg":
+        return "#EEEEEE";
+    }
+  }; //Closes TextColor method
 
-    const TextColor = () => {
-        switch (contrast) {
-            case 'sm':
-            case "md":
-                return theme === 'dark' ? '#EEEEEE' : '#333333';
-            case "lg":
-                return '#EEEEEE';
-        }
-    }//Closes TextColor method
-
-    return <div >
-        {type == "isotype" ?
-            <Isotype size={size} strokeColor={StrokeColor()} />
-            :
-            <Logotype size={size} strokeColor={StrokeColor()} textColor={TextColor()} />}
+  return (
+    <div>
+      {type == "isotype" ? (
+        <Isotype size={size} strokeColor={StrokeColor()} />
+      ) : (
+        <Logotype
+          size={size}
+          strokeColor={StrokeColor()}
+          textColor={TextColor()}
+        />
+      )}
     </div>
-}
+  );
+};
 //Closes Payosky Component
-
 
 export default PayoskyLogo;
